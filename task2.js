eddf0796d1089903c3a8a273ff7c0780836e3674
Vladimir Kormakov <cenizo.kht@gmail.com>
2021-10-10 22:52:30 +0300

    feat: add task2

---
+++ task2.js
@@ -0,0 +1,9 @@
+import fs from 'fs';
+import csv from 'csvtojson';
+
+const filePath = './nodejs-hw1-ex1.csv';
+
+const readableStream = fs.createReadStream(filePath, 'utf-8');
+const writableStream = fs.createWriteStream('outputFile.txt');
+
+readableStream.pipe(csv()).pipe(writableStream);
