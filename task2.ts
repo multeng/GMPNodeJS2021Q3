ff7ae6475c995681b49689564cb84c7f762081d8
Vladimir Kormakov <cenizo.kht@gmail.com>
2021-11-18 13:19:12 +0300

    feat: rewrite tasks with typescript

--- task2.ts
+++ task2.ts
@@ -6,4 +6,10 @@ const filePath = './nodejs-hw1-ex1.csv';
 const readableStream = fs.createReadStream(filePath, 'utf-8');
 const writableStream = fs.createWriteStream('outputFile.txt');
 
-readableStream.pipe(csv()).pipe(writableStream);
+readableStream
+  .pipe(csv())
+  .pipe(writableStream)
+  .on('error', (error) => {
+    console.error(error.message);
+    process.exit(1);
+  });
