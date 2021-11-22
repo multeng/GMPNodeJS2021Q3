a0d60485233672b962b414d0b197c8e89de15c19
Vladimir Kormakov <cenizo.kht@gmail.com>
2021-11-22 11:22:28 +0300

    refactor: fix some code

--- task2.ts
+++ task2.ts
@@ -1,28 +1,28 @@
 import fs from 'fs';
 import csv from 'csvtojson';
 
-
 type Book = {
-  Book: string,
-  Author: string,
-  Amount: string,
-  Price: string
-}
+  Book: string;
+  Author: string;
+  Amount: string;
+  Price: string;
+};
 
 const filePath = './hw1-ex1.csv';
 
 const readableStream = fs.createReadStream(filePath, 'utf-8');
 const writableStream = fs.createWriteStream('outputFile.txt');
 
-const addToDB = (data: Book) => {
-  new Promise ((resolve) => {
-    setTimeout(() => resolve(data), 1000)
-  })
-}
+const addToDB = (data: Book): void => {
+  new Promise((resolve) => {
+    console.log(data);
+    setTimeout(() => resolve(data), 1000);
+  });
+};
 
 readableStream
   .pipe(csv())
-  .subscribe((data: Book) => addToDB(data))
+  .subscribe(addToDB)
   .pipe(writableStream)
   .on('error', (error) => {
     console.error(error.message);
